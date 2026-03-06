fafb070afb6870eb4b450584438929fb9acb312e
Khushi2325 <146528176+Khushi2325@users.noreply.github.com>
2026-03-06 22:27:16 +0530

    Time: 5 ms (77.93%) | Memory: 47.4 MB (11.11%) - LeetSync

--- 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.java
+++ 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] minDistinctFreqPair(int[] nums) {
-        int freq[]=new int[101];
+        int[] fre = new int[101];
         Arrays.sort(nums);
-        for(int i:nums){
-            freq[i]++;
+        for(int i = 0; i<nums.length; i++){
+            fre[nums[i]]++;
         }
-        int min=nums[0];
-        int minfreq=freq[min];
-        for(int i:nums){
-            if(i!=min&&minfreq!=freq[i]){
-                return new int[]{min,i};
+
+        int min = nums[0];
+        int minfre = fre[min];
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i] != min && fre[nums[i]] != minfre){
+                return new int[]{min, nums[i]};
             }
         }
+
         return new int[]{-1,-1};
-        
+
+
     }
 }
