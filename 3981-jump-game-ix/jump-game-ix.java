8ffebb95ce56b4ad0fc5b02e8280127d4a8c99e9
Khushi2325 <146528176+Khushi2325@users.noreply.github.com>
2026-05-07 13:41:25 +0530

    Time: 4 ms (100.00%) | Memory: 198.8 MB (56.45%) - LeetSync

--- 3981-jump-game-ix/jump-game-ix.java
+++ 3981-jump-game-ix/jump-game-ix.java
@@ -1,34 +1,14 @@
 class Solution {
-
-    record Item(int value, int left, int right) {}
-
     public int[] maxValue(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
+        int n = nums.length, min = Integer.MAX_VALUE;
+        int[] max = new int[n], ans = new int[n];
+        max[0] = nums[0];
+        for(int i = 1; i < n; i++) max[i] = Math.max(max[i - 1], nums[i]);
 
-        List<Item> stack = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            Item curr = new Item(nums[i], i, i);
-
-            while (!stack.isEmpty() && stack.getLast().value() > nums[i]) {
-                Item top = stack.removeLast();
-                curr = new Item(
-                    Math.max(curr.value(), top.value()),
-                    top.left(),
-                    curr.right()
-                );
-            }
-
-            stack.add(curr);
+        for(int i = n - 2; i >= 0; i--) {
+            min = Math.min(min, nums[i + 1]);
+            if(min < max[i]) max[i] = max[i + 1];
         }
-
-        for (int i = 0; i < stack.size(); i++) {
-            for (int j = stack.get(i).left(); j <= stack.get(i).right(); j++) {
-                ans[j] = stack.get(i).value();
-            }
-        }
-
-        return ans;
+        return max;
     }
 }
