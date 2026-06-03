23cba5f854cf2782fb0dab2249ad92d9ad00fba1
Khushi2325 <146528176+Khushi2325@users.noreply.github.com>
2026-06-04 00:03:02 +0530

    Time: 6 ms (8.79%) | Memory: 47.3 MB (32.51%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,29 +1,25 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-    }
-    public int minDistance(String word1, String word2) {
-        int n = word1.length();
-        int m = word2.length();
+    public int minDistance(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
 
         int[][] dp = new int[n+1][m+1];
 
         for(int i = 0; i<n+1; i++){
-            dp[i][0] = i;
+            for(int j =0; j<m+1; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }
+
+                if(j == 0){
+                    dp[i][j] = i;
+                }
+            }
         }
 
-        for(int j = 0; j<m+1; j++){
-            dp[0][j] = j;
-        }
-
-        for(int i = 1; i<n+1; i++){
+        for(int i=1; i<n+1; i++){
             for(int j = 1; j<m+1; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
                     dp[i][j] = dp[i-1][j-1];
                 }else{
                     dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1])) + 1;
