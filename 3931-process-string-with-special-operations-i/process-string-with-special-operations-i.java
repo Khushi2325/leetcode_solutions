8de12984f2d0765c99a842c9500f1ac4731f3e13
Khushi2325 <146528176+Khushi2325@users.noreply.github.com>
2026-06-16 08:00:51 +0530

    Time: 3 ms (100.00%) | Memory: 55.4 MB (48.53%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -5,12 +5,13 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         for(int i = 0; i<n; i++){
-            if(Character.isLetter(s.charAt(i))){
-                sb.append(s.charAt(i));
-            }else if(s.charAt(i) == '*'){
+            char ch = s.charAt(i);
+            if(Character.isLowerCase(ch)){
+                sb.append(ch);
+            }else if(ch == '*'){
                 if(sb.length() == 0) continue;
-                sb.deleteCharAt(sb.length() - 1);
-            }else if(s.charAt(i) == '#'){
+                sb.deleteCharAt(sb.length() -1);
+            }else if(ch == '#'){
                 sb.append(sb.toString());
             }else{
                 sb.reverse();
