24a5968191f63112703677a4db5d4f4de409c583
Khushi2325 <146528176+Khushi2325@users.noreply.github.com>
2026-08-27 10:58:21 +0530

    Time: 4 ms (91.40%) | Memory: 46.9 MB (88.40%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,12 +1,25 @@
-class Solution {
-    public boolean canFinish(int n, int[][] edges) {
-        boolean[] vis = new boolean[n];
-        boolean[] recPath = new boolean[n];
+import java.util.*;
 
-        for(int i =0; i<n; i++){
-            if(!vis[i]){
-                if(isCycle(i, vis, recPath, edges)){
-                    return false;
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int[] edge : prerequisites) {
+            int v = edge[0]; 
+            int u = edge[1]; 
+            adj.get(u).add(v); // u -> v
+        }
+
+        boolean[] vis = new boolean[numCourses];
+        boolean[] recPath = new boolean[numCourses];
+
+        // 3. Run DFS for each component: O(V + E) total
+        for (int i = 0; i < numCourses; i++) {
+            if (!vis[i]) {
+                if (isCycle(i, vis, recPath, adj)) {
+                    return false; // Cycle detected
                 }
             }
         }
@@ -14,22 +27,17 @@ class Solution {
         return true;
     }
 
-    public boolean isCycle(int src, boolean[] vis, boolean[] recPath, int[][] edges){
+    private boolean isCycle(int src, boolean[] vis, boolean[] recPath, List<List<Integer>> adj) {
         vis[src] = true;
         recPath[src] = true;
 
-        for(int i = 0; i<edges.length; i++){
-            int v = edges[i][0];
-            int u = edges[i][1];
-
-            if(u == src){
-                if(!vis[v]){
-                    if(isCycle(v, vis, recPath, edges)){
-                        return true;
-                    }
-                }else if(recPath[v]){
+        for (int neighbor : adj.get(src)) {
+            if (!vis[neighbor]) {
+                if (isCycle(neighbor, vis, recPath, adj)) {
                     return true;
                 }
+            } else if (recPath[neighbor]) {
+                return true;
             }
         }
 
