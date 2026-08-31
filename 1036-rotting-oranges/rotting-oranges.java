4e04bd96c35ad28e83c4936e2e2e0af2ede21786
Khushi2325 <146528176+Khushi2325@users.noreply.github.com>
2026-08-31 22:37:06 +0530

    Time: 2 ms (89.40%) | Memory: 44.1 MB (72.19%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,69 +1,64 @@
 class Solution {
     public class Node{
-        int row;
-        int col;
-        int time;
+        int r;
+        int c;
+        int t;
 
-        Node(int a, int b, int c){
-            this.row = a;
-            this.col = b;
-            this.time = c;
+        Node(int r, int c, int t){
+            this.r = r;
+            this.c = c;
+            this.t = t;
         }
     }
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int ans = 0;
-        boolean[][] vis = new boolean[n][m];
-
         Queue<Node> q = new LinkedList<>();
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[0].length; j++){
                 if(grid[i][j] == 2){
-                    q.add(new Node(i,j,0));
+                    q.add(new Node(i, j, 0));
                 }
             }
         }
 
+        int ans = 0;
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+
         while(!q.isEmpty()){
-            Node curr = q.remove();
+            Node cur = q.remove();
 
-            int i = curr.row;
-            int j = curr.col;
-            int t = curr.time;
+            int i = cur.r;
+            int j = cur.c;
+            int t = cur.t;
 
-            ans = Math.max(ans, t);
+            ans = Math.max(t, ans);
 
-            //top
+            
+            ///top(i-1, j)
             if(i-1 >= 0 && grid[i-1][j] == 1 && !vis[i-1][j]){
-                q.add(new Node(i-1, j, t+1));
                 vis[i-1][j] = true;
+                q.add(new Node(i-1, j, t+1));
             }
 
-            //left
-            if(j-1 >= 0 && grid[i][j-1] == 1 && !vis[i][j-1]){
-                q.add(new Node(i, j-1, t+1));
-                vis[i][j-1] = true;
-            }
-
-            //right
-            if(j+1 < m && grid[i][j+1] == 1 && !vis[i][j+1]){
-                q.add(new Node(i, j+1, t+1));
-                vis[i][j+1] = true;
-            }
-
-            //bottom
-            if(i+1 < n && grid[i+1][j] == 1 && !vis[i+1][j]){
-                q.add(new Node(i+1, j, t+1));
+            if(i+1 < grid.length && grid[i+1][j] == 1 && !vis[i+1][j]){
                 vis[i+1][j] = true;
+                q.add(new Node(i+1, j, t+1));
+            }
+
+            if(j-1 >= 0 && grid[i][j-1] == 1 && !vis[i][j-1]){
+                vis[i][j-1] = true;
+                q.add(new Node(i, j-1, t+1));
+            }
+
+            if(j+1 < grid[0].length && grid[i][j+1] == 1 && !vis[i][j+1]){
+                vis[i][j+1] = true;
+                q.add(new Node(i, j+1, t+1));
             }
         }
 
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(grid[i][j] == 1 && !vis[i][j]){
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[0].length; j++){
+                if(!vis[i][j] && grid[i][j] == 1){
                     return -1;
                 }
             }
